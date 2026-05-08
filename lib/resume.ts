f0d35cd8bb06fd8f28d3acf850f7c5e264974352
annylin-tingyu <annylin.tingyu@gmail.com>
2026-05-08 10:48:11 -0700

    Updates

--- lib/resume.ts
+++
@@ -1,4 +0,0 @@
-/** Filename in /public; encoded for use in Link href (Next.js applies basePath in production). */
-const RESUME_FILENAME = "2026 Anny_Lin Resume.pdf";
-
-export const RESUME_PDF_HREF = `/${encodeURIComponent(RESUME_FILENAME)}`;
